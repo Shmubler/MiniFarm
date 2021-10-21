557674a2d7f72798512aa796dffd120b81108f43
Nicholas Chan <74500036+n-chan@users.noreply.github.com>
2021-10-20 18:48:24 -0700

    Edited GameScene. Tiles can be changed on click.
    Art Sprites by Sarah E.

---
+++ Mini Farm/Assets/Scripts/ChangeTileOnClick.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChangeTileOnClick : MonoBehaviour
+{
+    public Grid grid;
+    public Tilemap ground;
+    public Tile tile;
+
+    bool isTileChangeable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTileChangeable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleTileChange() {
+        isTileChangeable = !isTileChangeable;
+    }
+
+    void OnMouseDown() {
+        if (isTileChangeable) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            ground.SetTile(position, tile);
+        }
+    }
+}
